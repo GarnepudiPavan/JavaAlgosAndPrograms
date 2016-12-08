c4267978e1af1bb4e16f048a0e743304bb74ee28
Pavan <Pavan@SIRIPAVAN>
2016-12-09 01:04:28 +0530

    Added traversal (inorder, pre and post order) in Binary trees and Deletion of child nodes. Also included is finding nth smallest element in Binary tree.

--- DailyJava/src/com/daily/java/DataStructures/BinaryTreeImp.java
+++ DailyJava/src/com/daily/java/DataStructures/BinaryTreeImp.java
@@ -1,12 +1,12 @@
 package com.daily.java.DataStructures;
 
-public class TreesImp {
+public class BinaryTreeImp {
 
 	Node root;
 
 	public static class Node {
 
-		int iData;
+		int key;
 		int data;
 		Node leftChild;
 		Node rightChild;
@@ -15,25 +15,25 @@ public class TreesImp {
 
 		}
 
-		public Node(int iData, int data) {
-			this.iData = iData;
+		public Node(int key, int data) {
+			this.key = key;
 			this.data = data;
 		}
 
 		public Node(int key) {
-			this.iData = key;
+			this.key = key;
 		}
 
 		public void setKey(int key) {
-			this.iData = key;
+			this.key = key;
 		}
 
 		public int getKey() {
-			return this.iData;
+			return this.key;
 		}
 
 		public void display() {
-			System.out.println("Node key is" + " " + this.iData);
+			System.out.println("Node key is" + " " + this.key);
 			System.out.println("Node value is" + " " + this.data);
 		}
 
@@ -49,7 +49,7 @@ public class TreesImp {
 	public void addNode(Node newNode) {
 		// newNode = new Node();// New node instance to be used if Parent node
 		// is null
-		int key = newNode.iData;
+		int key = newNode.key;
 		// int data=newNode.data;
 		if (root == null) {
 			root = newNode;
@@ -59,8 +59,8 @@ public class TreesImp {
 			Node parent;
 			while (true) {
 				parent = current;
-				if (key < current.iData) {
-					current = current.leftChild;
+				if (key < parent.key) {
+					current = parent.leftChild;
 					// If left child of current parent node is null then insert
 					// a new node
 					if (current == null) {
@@ -69,7 +69,7 @@ public class TreesImp {
 						return;
 					}
 				} else {
-					current = current.rightChild;
+					current = parent.rightChild;
 					// If right child of current parent node is null then insert
 					// a new node
 					if (current == null) {
@@ -86,7 +86,7 @@ public class TreesImp {
 	public void insert(int key, int data) {
 		Node newNode = new Node();// New node instance to be used if Parent node
 									// is null
-		newNode.iData = key;
+		newNode.key = key;
 		newNode.data = data;
 		if (root == null) {
 			root = newNode;
@@ -96,7 +96,7 @@ public class TreesImp {
 			Node parent;
 			while (true) {
 				parent = current;
-				if (key < current.iData) {
+				if (key < parent.key) {
 					current = current.leftChild;
 					// If left child of current parent node is null then insert
 					// a new node
@@ -120,41 +120,82 @@ public class TreesImp {
 
 	}
 
-	public void delete() {
+	public void delete(Node node) {
+
+		if (root == null) {
+			return;
+		} else {
+			Node current = root;
+			Node parent = null;
+			boolean isLeftChild = true;
+			while (current.key != node.key) {
+				parent = current;
+				if (node.key < current.key) {
+					isLeftChild = true;
+					current = current.leftChild;
+				} else {
+					isLeftChild = true;
+					current = current.rightChild;
+				}
+
+			}//Deletion of node which has no children that is leaf nodes
+			if (current.leftChild == null && current.rightChild == null) {// to
+																			// ensure
+																			// node
+																			// are
+																			// having
+																			// no
+																			// child
+				if (current == root) {// if node found is root then delete root
+					root = null;
+				}
+				if (isLeftChild) {// if node funs is left child then set its parent's
+							// leftChild link to null. Then leftChild will be
+							// collected by GC
+					parent.leftChild = null;
+				} else { // else set Parent rightChild to null
+					parent.rightChild = null;
+				}
+			}
+		}
 
 	}
 
-	public int findMin(Node current) {
-		
-		 Node last = null;
-		 current = root;
+	public int findMin(Node current, int n) {
+		int count = -1;
+		Node last = null;
+		current = root;
 		if (current != null) {
-			
+
 			while (current != null) {
-				last=current;
+				count++;
+
+				last = current;
 				current = current.leftChild;
-				
-				//System.out.println("Min value is" + " " + last.getKey()+" "+last.data);
+
+				if (count == n) {
+					return last.data;
+				}
 			}
-			 return last.data;
+			return last.data;
 		} else {
 			return -1;
 		}
 
 	}
 
-	
 	public int findMax(Node current) {
 		Node last = null;
 		current = root;
 		if (current != null) {
-			while (current!= null) {
-				last=current;
+			while (current != null) {
+				last = current;
 				current = current.rightChild;
-				
-				//System.out.println("Max value is" + " " + last.getKey()+" "+last.data);
+
+				// System.out.println("Max value is" + " " + last.getKey()+"
+				// "+last.data);
 			}
-			 return last.data;
+			return last.data;
 		} else {
 			return -1;
 		}
@@ -162,12 +203,12 @@ public class TreesImp {
 	}
 
 	// Time taken to find a node in Tree is O(logN)
-	public Node findKey(int key) {
+	public Node findNode(int key) {
 
 		Node current = root;
 
-		while (current.iData != key) {
-			if (key < current.iData) {
+		while (current.key != key) {
+			if (key < current.key) {
 				current = current.leftChild;
 			} else {
 				current = current.rightChild;
@@ -180,45 +221,79 @@ public class TreesImp {
 		return current;
 	}
 
-	// traversing a tree
+	// traversing a tree in Inorder which travers in increasing sort order
 
 	private void inOrder(Node localRoot) {
-		System.out.println("traversing....");
+		// System.out.println("traversing in Inorder <LeftChild> <Root>
+		// <RightChild>....");
 		if (localRoot != null) {
 			inOrder(localRoot.leftChild);
-			System.out.println("Node Key" + " : " + localRoot.iData + " ");
-			System.out.print("Node value" + " : " + localRoot.data + " ");
+			System.out.println("Node Key" + " : " + localRoot.key + " ");
+			System.out.println("Node value" + " : " + localRoot.data + " ");
 			inOrder(localRoot.rightChild);
+		}
+	}
+
+	// traversing a tree
+
+	private void postOrder(Node localRoot) {
+		// System.out.println("traversing in Post Order <LeftChilD> <RightChild>
+		// <Root>....");
+		if (localRoot != null) {
+			postOrder(localRoot.leftChild);
+			postOrder(localRoot.rightChild);
+			System.out.println("Node Key" + " : " + localRoot.key + " ");
+			System.out.println("Node value" + " : " + localRoot.data + " ");
+
+		}
+	}
+
+	// traversing a tree
+
+	private void preOrder(Node localRoot) {
+		// System.out.println("traversing in PreOrder is <Root> <LeftChild>
+		// <RightChild>....");
+		if (localRoot != null) {
+			System.out.println("Node Key" + " : " + localRoot.key + " ");
+			System.out.println("Node value" + " : " + localRoot.data + " ");
+			preOrder(localRoot.leftChild);
+			preOrder(localRoot.rightChild);
+
 		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TreesImp obj1 = new TreesImp();
+		BinaryTreeImp obj1 = new BinaryTreeImp();
 		Node root = new Node(10, 101);
-		Node root1 = new Node(4, 98);
-		Node root2 = new Node(6, 99);
-		Node root4 = new Node(7, 100);
+		Node root1 = new Node(6, 99);
+		Node root2 = new Node(4, 98);
+		Node root3 = new Node(7, 100);
+		Node root4 = new Node(12, 401);
 		Node root5 = new Node(11, 400);
-		Node root6 = new Node(12, 401);
-		Node root7 = new Node(13, 402);
+		Node root6 = new Node(13, 402);
 		obj1.addNode(root);
 		obj1.addNode(root1);
 		obj1.addNode(root2);
+		obj1.addNode(root3);
 		obj1.addNode(root4);
 		obj1.addNode(root5);
 		obj1.addNode(root6);
-		obj1.addNode(root7);
 
 		// obj1.insert(root3, 31,32);
 		// obj1.insert(root4, 41,42);
+		System.out.println("traversing in Inorder <LeftChild> <Root> <RightChild>....");
 		obj1.inOrder(root);
-		/*System.out.println("Inserting new nodes....");
-		obj1.insert(3, 12);
-		obj1.insert(21, 220);
-		obj1.inOrder(root);*/
-		System.out.println("Min value is" + " " + obj1.findMin(root));
-		System.out.println("Max value is" + " " + obj1.findMax(root));
+		System.out.println("traversing in Post-Order <LeftChild> <RightChild> <Root> ....");
+		obj1.postOrder(root);
+		System.out.println("traversing in Pre-Order <Root> <LeftChild> <RightChild>  ....");
+		obj1.preOrder(root);
+		/*
+		 * System.out.println("Inserting new nodes...."); obj1.insert(3, 12);
+		 * obj1.insert(21, 220); obj1.inOrder(root);
+		 */
+		System.out.println("Find nth Min value in tree" + " " + obj1.findMin(root, 3));
+		// System.out.println("Max value is" + " " + obj1.findMax(root));
 	}
 
 }
