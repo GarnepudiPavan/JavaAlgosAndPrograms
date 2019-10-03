c4e2da63d2f7ad390c1c4598fafdc2a5509b0f30
Garnepudi <gvpk18@gmail.com>
2019-10-03 21:09:12 +0530

    Added logic to check given strings are anagram without Sort using Maps.

--- Practise/src/com/daily/practise/Anagram.java
+++ Practise/src/com/daily/practise/Anagram.java
@@ -1,8 +1,11 @@
 package com.daily.practise;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Anagram {
 
@@ -28,6 +31,8 @@ public class Anagram {
 					counter++;
 				}
 			}
+			System.out.println("charArray length is" + " " + charArray.length);
+			System.out.println("String2 length is.." + " " + string2.length());
 			System.out.println("Counter value is.." + " " + counter);
 			System.out.println("CharArray and String1 length value is.." + " " + charArray.length + " "
 					+ string1.length() + "resp");
@@ -55,12 +60,56 @@ public class Anagram {
 		}
 	}
 
+	public boolean anaCheckWithMapAndNoSort() {
+		boolean isAnagram;
+		if(string1.length()==string2.length()) {
+		Map<String, Integer> list1 = new LinkedHashMap<String, Integer>();
+		Map<String, Integer> list2 = new LinkedHashMap<String, Integer>();
+		int counter=0;
+		for (int i = 0; i < string1.length(); i++) {
+			if(list1.containsKey(string1.charAt(i))) {
+			list1.put(String.valueOf(string1.charAt(i)), counter+1);
+			}else {
+				list1.put(String.valueOf(string1.charAt(i)), counter);
+			}
+		}
+		for (int i = 0; i < string2.length(); i++) {
+			if(list2.containsKey(string2.charAt(i))) {
+			list2.put(String.valueOf(string2.charAt(i)), counter+1);
+			}else {
+				list2.put(String.valueOf(string2.charAt(i)), counter);
+			}
+		}
+		for (Map.Entry<String, Integer> values : list1.entrySet()) {
+			System.out.println("Char from list1" + " " + values.getKey());
+			System.out.println("Values from list1" + " " + values.getValue());
+		}
+		for (Map.Entry<String, Integer> values : list2.entrySet()) {
+			System.out.println("Char from list1" + " " + values.getKey());
+			System.out.println("Values from list1" + " " + values.getValue());
+		}
+		if(list1.equals(list2)) {
+			System.out.println("Given Strings" + " " + string1+" "+string2+" "+"are Anagram");
+			isAnagram = true;
+		}else {
+			System.out.println("Given Strings" + " " + string1+" "+string2+" "+"are NOT Anagram");
+			isAnagram=false;
+		}}
+		else {
+			System.out.println("Given Strings" + " " + string1+" "+string2+" "+"are NOT Anagram");
+			isAnagram = false;
+		}
+		return isAnagram;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Anagram object1 = new Anagram("Is he Aradhya", "he is hradaya");
+		Anagram object1 = new Anagram("Aradhya", "hradaya");
 		object1.checkAnagram();
 		object1.checkAnagramusingSort();
 		System.out.println("Time taken is" + " " + (object1.endTime - object1.startTime) + "ms");
+		object1.anaCheckWithMapAndNoSort();
 	}
 
 }
